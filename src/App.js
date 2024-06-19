78a0a71f6d5eba867e89e8c68cf0e11ed3093eaf
saurav tiwari <sauravtiwari145@gmail.com>
2024-06-19 23:56:46 +0545

    homepage hero design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'animate.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './components/Home'
 import Teachers from './components/Teachers'
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,24 @@ const Home = () => {
   return (
     <div>
         <Nav/>
-        <h1>Home</h1>
+        <div>
+          <header className='grid grid-cols-2 p-24 bg-gradient-to-r from-indigo-500 animate__animated animate__fadeIn'>
+            <div>
+            <img 
+                  src="./images/student.jpg" alt="student" 
+                  className='w-3/4 rounded-full animate__animated animate__pulse border border-2 shadow-xl'
+              />
+            </div>
+            <div>
+             <h1>Say Hello !</h1>
+             <p>Enjoy complete digital learning experience</p>
+             <div>
+              <button>Learn More</button>
+              <button>Watch Video</button>
+             </div>
+            </div>
+          </header>
+        </div>
         <Footer/>
     </div>
   )
